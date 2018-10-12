1b179b775c31eec5ecf554b89881253d58b7b6ab
Rubén Zúñiga García <rbnzunigagarcia@gmail.com>
2018-10-12 17:20:02 +0200

    Fixed bug

--- app/src/main/java/com/example/damian/acdat/ej1_activity.java
+++ app/src/main/java/com/example/damian/acdat/ej1_activity.java
@@ -1,9 +1,10 @@
 package com.example.damian.acdat;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ej1_activity extends AppCompatActivity {
+public class ej1_activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
